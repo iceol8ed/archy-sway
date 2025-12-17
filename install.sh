f0ba8dfa35c5be513c612a0375a7fd165d872a1c
iceol8ed <t.leonidius@gmail.com>
2025-12-17 09:17:02 +0200

    ship it

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ mkinitcpio -P
 echo "[*] Installing base packages ..."
 retry_cmd pacman -S --noconfirm --needed \
   sway autotiling-rs wtype polkit-kde-agent xdg-desktop-portal-wlr xdg-desktop-portal-gtk wl-clipboard mpv \
-  bluetui nvim foot jq swaybg grim slurp xorg-xwayland wl-clip-persist fuzzel cliphist fastfetch \
+  bluetui nvim foot jq fd swaybg grim slurp xorg-xwayland wl-clip-persist fuzzel cliphist fastfetch \
   deluge-gtk btop zip unzip zsh ttf-jetbrains-mono ttf-jetbrains-mono-nerd \
   noto-fonts noto-fonts-emoji noto-fonts-cjk curl wget base-devel yazi wiremix
 
